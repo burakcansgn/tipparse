7825ae395e4a6d1acb33781bacc1510fd07f8e1a
Burakcan <burakcansgn@gmail.com>
2016-11-14 16:50:38 +0200

    BasicDataClearinVenue

---
+++ MyLib/BasicDataClearingVenue.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    public class BasicDataClearingVenue
+    {
+        public readonly int i;
+        public readonly string Si;
+        public readonly int s;
+        public readonly string SYm;
+        public readonly string NAm;
+
+        public BasicDataClearingVenue(string name)
+        {
+            string[] words = name.Split(';');
+            i = Int32.Parse(words[1].Substring(1, words[1].Length - 1));
+            Si = words[2].Substring(2, words[2].Length - 2);
+            s = Int32.Parse(words[3].Substring(1, words[3].Length - 1));
+            SYm = words[4].Substring(3, words[4].Length - 3);
+            NAm = words[5].Substring(3, words[5].Length - 3);
+        }
+    }
+}
---
+++ TestMyLib/BasicDataClearingVenueTest.cs
@@ -0,0 +1,27 @@
+﻿using MyLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMyLib
+{
+    [TestFixture]
+    class BasicDataClearingVenueTest
+    {
+        [Test]
+        public void generateclass7()
+        {
+            string data = "BDCv;i250;SiTVS;s1;SYmTVSBTRISXXX;NAmTAKASBANK;";
+            BasicDataClearingVenue BDCv = new BasicDataClearingVenue(data);
+            Assert.AreEqual(250,BDCv.i);
+            Assert.AreEqual("TVS",BDCv.Si);
+            Assert.AreEqual(1,BDCv.s);
+            Assert.AreEqual("TVSBTRISXXX",BDCv.SYm);
+            Assert.AreEqual("TAKASBANK",BDCv.NAm);
+            
+        }
+    }
+}
