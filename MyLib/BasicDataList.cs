e2bc396c59a0c997e4afb3e93b91f0192229f8c8
Burakcan <burakcansgn@gmail.com>
2016-11-21 17:25:25 +0200

    BasicDataList

--- MyLib/BasicDataList.cs
+++ MyLib/BasicDataList.cs
@@ -8,6 +8,46 @@ namespace MyLib
 {
     public class BasicDataList
     {
+        public readonly int i;
+        public readonly string Si;
+        public readonly int s;
+        public readonly int PAi;
+        public readonly string SYm;
+        public readonly string NAm;
+        public readonly string LCy;
+        public readonly Boolean TCe;
+
+        public BasicDataList(string name)
+        {
+            string[] words = name.Split(';');
+            i = Int32.Parse(words[1].Substring(1, words[1].Length - 1));
+            Si = words[2].Substring(2, words[2].Length - 2);
+            s = Int32.Parse(words[3].Substring(1, words[3].Length - 1));
+            if ((words[4].Substring(0, 3) != "PAi")) {
+                SYm = (words[4].Substring(3, words[4].Length - 3));
+                NAm = words[5].Substring(3, words[5].Length - 3);
+                LCy = words[6].Substring(3, words[6].Length - 3);
+                if (words[7].Substring(3, words[7].Length - 3) == "Y")
+                    TCe = true;
+                 else
+                TCe = false;
+            
+        }
+        else
+            {
+                PAi = Int32.Parse(words[4].Substring(3, words[4].Length - 3));
+                SYm = (words[5].Substring(3, words[5].Length - 3));
+                NAm = words[6].Substring(3, words[6].Length - 3);
+                LCy = words[7].Substring(3, words[7].Length - 3);
+                if (words[8].Substring(3, words[8].Length - 3) == "Y")
+                    TCe = true;
+                else
+                    TCe = false;
+            }
+        }
+
+
+
        
     }
 }
--- TestMyLib/BasicDataListTest.cs
+++ TestMyLib/BasicDataListTest.cs
@@ -14,8 +14,16 @@ namespace TestMyLib
         [Test]
         public void generateclass12()
         {
-            string data = "BDLi;i8;Si080;s1;SYmXIST;NAmBorsa Istanbul;LCyTRY;TCeY;";
-
+            string data = "BDLi;i258;Si080160;s1;PAi8;SYmPDATA;NAmMARKET FOR EXTERNAL PRICE DATA;LCyTRY;TCeY;";
+            BasicDataList BDLi = new BasicDataList(data);
+            Assert.AreEqual(258,BDLi.i);
+            Assert.AreEqual("080160", BDLi.Si);
+            Assert.AreEqual(1, BDLi.s);
+            Assert.AreEqual(8, BDLi.PAi);
+            Assert.AreEqual("PDATA", BDLi.SYm);
+            Assert.AreEqual("MARKET FOR EXTERNAL PRICE DATA", BDLi.NAm);
+            Assert.AreEqual("TRY", BDLi.LCy);
+            Assert.AreEqual(true, BDLi.TCe);
         }
     }
 }
