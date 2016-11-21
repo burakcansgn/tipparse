50f24662eb446103246cf0f1ba1e30d93123d923
Burakcan <burakcansgn@gmail.com>
2016-11-21 15:01:56 +0200

    BasicDataTradingScheme

---
+++ MyLib/BasicDataTradingScheme.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    public class BasicDataTradingScheme
+    {
+        public readonly int s;
+        public readonly int TRId;
+        public readonly string Si;
+        public readonly DateTime Dt;
+        public readonly string Ms;
+        public readonly TimeSpan St;
+
+        public BasicDataTradingScheme(string name)
+        {
+            string[] words = name.Split(';');
+            s = Int32.Parse(words[1].Substring(1, words[1].Length - 1));
+            TRId = Int32.Parse(words[2].Substring(4, words[2].Length - 4));
+            Si = words[3].Substring(2, words[3].Length - 2);
+            Dt = DateTime.ParseExact(words[4].Substring(2, words[4].Length - 2),"yyyyMMdd", CultureInfo.InvariantCulture);
+            Ms = words[5].Substring(2, words[5].Length - 2);
+            St = TimeSpan.ParseExact(words[6].Substring(2, words[6].Length - 2), "hhmmss\\.fff",CultureInfo.InvariantCulture);
+
+        }
+    }
+}
---
+++ TestMyLib/BasicDataTradingSchemeTest.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using MyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace TestMyLib
+{
+    [TestFixture]
+    class BasicDataTradingSchemeTest
+    {
+        [Test]
+         public void deneme()
+        {
+            string se = "20150730";
+            DateTime dt = DateTime.ParseExact(se, "yyyyMMdd", CultureInfo.InvariantCulture);
+        }
+        [Test]
+        public void generateclass11()
+        {
+            TimeSpan ts = new TimeSpan(0, 17, 40, 00, 000);
+             DateTime t = new DateTime(2015, 07, 30);
+             string data = "BDTm;s1;TRId96;SiP_SALT_SI;Dt20150730;Ms1;St174000.000;";
+             BasicDataTradingScheme BDTm = new BasicDataTradingScheme(data);
+             Assert.AreEqual(1, BDTm.s);
+             Assert.AreEqual(96,BDTm.TRId);
+             Assert.AreEqual("P_SALT_SI",BDTm.Si);
+             Assert.AreEqual(t,BDTm.Dt);
+             Assert.AreEqual("1", BDTm.Ms);
+             Assert.AreEqual(ts,BDTm.St);
+             
+            
+        }
+
+    }
+}
