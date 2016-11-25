3efcd89d3aa9da53f61f04aa5a22d6c6ecbf84d4
Burakcan <burakcansgn@gmail.com>
2016-11-25 13:54:59 +0200

    BasicDataSector

---
+++ MyLib/BasicDataSector.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    public class BasicDataSector
+    {
+        public readonly int i;
+        public readonly string Si;
+        public readonly int s;
+        public readonly string SYm;
+        public readonly string NAm;
+        public readonly int CDLv;
+        public readonly int PAi;
+        private string[] checks = { "i", "Si", "s", "SYm", "NAm", "CDLv", "PAi" };
+
+        public BasicDataSector(string name)
+        {
+            string[] words = name.Split(';');
+            foreach (string word in words)
+            {
+                foreach (string check in checks)
+                {
+
+                    if (word.StartsWith(checks[0]))
+                        i = Int32.Parse(word.Substring(1, word.Length - 1));
+                    else if (word.StartsWith(checks[1]))
+                        Si = word.Substring(2, word.Length - 2);
+                    else if (word.StartsWith(checks[2]))
+                        s = Int32.Parse(word.Substring(1, word.Length - 1));
+                    else if (word.StartsWith(checks[3])) 
+                        SYm = word.Substring(3, word.Length - 3);
+                    else if (word.StartsWith(checks[4]))
+                        NAm = word.Substring(3, word.Length - 3);
+                    else if (word.StartsWith(checks[5]))
+                        CDLv = Int32.Parse(word.Substring(4, word.Length - 4));
+                    else if (word.StartsWith(checks[6]))
+                        PAi = Int32.Parse(word.Substring(3, word.Length - 3));
+                }
+            }
+
+
+        }
+    }
+}
+
---
+++ TestMyLib/BasicDataSectorTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using MyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMyLib
+{
+    [TestFixture]
+    class BasicDataSectorTest
+    {
+        [Test]
+        public void generateclass13()
+        {
+            string data = "BDs;i128;Si1410;s1;NAmGAYRIMENKUL FAALIYETLERI;CDLv1;";
+            BasicDataSector BDs = new BasicDataSector(data);
+            Assert.AreEqual(128, BDs.i);
+            Assert.AreEqual("1410", BDs.Si);
+            Assert.AreEqual(1, BDs.s);
+            Assert.AreEqual("GAYRIMENKUL FAALIYETLERI", BDs.NAm);
+            Assert.AreEqual(1, BDs.CDLv);
+
+
+        }
+        [Test]
+        public void generateclass14()
+        {
+            string data = "BDs;PAi8;i128;Si1410;s1;NAmGAYRIMENKUL FAALIYETLERI;SYmq;CDLv1;";
+            BasicDataSector BDs = new BasicDataSector(data);
+            Assert.AreEqual(128, BDs.i);
+            Assert.AreEqual("1410", BDs.Si);
+            Assert.AreEqual(1, BDs.s);
+            Assert.AreEqual("GAYRIMENKUL FAALIYETLERI", BDs.NAm);
+            Assert.AreEqual(1, BDs.CDLv);
+            Assert.AreEqual("q", BDs.SYm);
+            Assert.AreEqual(8, BDs.PAi);
+        }
+    }
+}
