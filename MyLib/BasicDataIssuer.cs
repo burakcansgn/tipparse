3a7ef722f769bda9fb67cb29308a341ea328c73e
Burakcan <burakcansgn@gmail.com>
2016-11-25 14:43:55 +0200

    BasicDataIssuer

---
+++ MyLib/BasicDataIssuer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    public class BasicDataIssuer
+    {
+        public readonly int i;
+        public readonly string Si;
+        public readonly int s;
+        public readonly string SYm;
+        public readonly string NAm;
+        public readonly string CNy;
+        private string[] checks = { "i", "Si", "s", "SYm", "NAm", "CNy" };
+
+        public BasicDataIssuer(string name)
+        {
+            string[] words = name.Split(';');
+            foreach (string word in words)
+            {
+                foreach (string check in checks)
+                {
+                    if (word.StartsWith(checks[0]))
+                        i = Int32.Parse(word.Substring(1, word.Length - 1));
+                    else if (word.StartsWith(checks[1]))
+                        Si = word.Substring(2, word.Length - 2);
+                    else if (word.StartsWith(checks[2]))
+                        s = Int32.Parse(word.Substring(1, word.Length - 1));
+                    else if (word.StartsWith(checks[3]))
+                        SYm = word.Substring(3, word.Length - 3);
+                    else if (word.StartsWith(checks[4]))
+                        NAm = word.Substring(3, word.Length - 3);
+                    else if (word.StartsWith(checks[5]))
+                        CNy = word.Substring(3, word.Length - 3);
+                }
+            }
+        }
+    }
+}
---
+++ TestMyLib/BasicDataIssuerTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using MyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMyLib
+{
+    [TestFixture]
+    class BasicDataIssuerTest
+    {
+        [Test]
+        public void generateclass15()
+        {
+            string data = "BDIs;i304;SiAFB;s1;SYmAFB;NAmAKTIF YATIRIM  BANKASI AS;CNyTR;";
+            BasicDataIssuer BDs = new BasicDataIssuer(data);
+            Assert.AreEqual(304, BDs.i);
+            Assert.AreEqual("AFB", BDs.Si);
+            Assert.AreEqual(1, BDs.s);
+            Assert.AreEqual("AKTIF YATIRIM  BANKASI AS", BDs.NAm);
+            Assert.AreEqual("TR", BDs.CNy);
+            Assert.AreEqual("AFB", BDs.SYm);
+
+        }
+    }
+}
