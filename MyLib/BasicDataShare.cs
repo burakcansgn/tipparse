480612e4e99bc0241724fe2697030ac5e17d4ee8
Burakcan <burakcansgn@gmail.com>
2016-11-28 17:58:11 +0200

    Basic Data Share

---
+++ MyLib/BasicDataShare.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    public class BasicDataShare
+    {
+        public readonly int i;
+        public readonly string Si;
+        public readonly int s;
+        public readonly Boolean HOt;
+        public readonly int AVq;
+        public readonly DateTime As;
+        public readonly DateTime Ae;
+        public readonly int ICl;
+        public readonly float TIs;
+        string[] checks = { "i", "Si", "s", "HOt", "AVq", "As", "Ae", "ICl", "TIs" };
+
+        public BasicDataShare(string name)
+        {
+
+            string[] words = name.Split(';');
+            foreach (string word in words)
+            {
+                foreach (string check in checks)
+                {
+                    if (word.StartsWith(checks[0]))
+                        i = Int32.Parse(word.Substring(1, word.Length - 1));
+                    else if (word.StartsWith(checks[1]))
+
+                        Si = word.Substring(2, words.Length-2);
+
+                    else if (word.StartsWith(checks[2]))
+                        s = Int32.Parse(word.Substring(1, word.Length - 1));
+                    else if (word.StartsWith(checks[3]))
+                    {
+                        if (word.Substring(3, word.Length - 3) == "N")
+                            HOt = false;
+                        else
+                            HOt = true;
+                    }
+                    else if (word.StartsWith(checks[4]))
+                        AVq = Int32.Parse(word.Substring(3, word.Length - 3));
+                    else if (word.StartsWith(checks[5]))
+                        As = DateTime.ParseExact(word.Substring(2, word.Length - 2), "yyyyMMdd", CultureInfo.InvariantCulture);
+                    else if (word.StartsWith(checks[6]))
+                        Ae = DateTime.ParseExact(word.Substring(2, word.Length - 2), "yyyyMMdd", CultureInfo.InvariantCulture);
+                    else if (word.StartsWith(checks[7]))
+                        ICl = Int32.Parse(word.Substring(3, word.Length - 3));
+                    else if (word.StartsWith(checks[8]))
+                        TIs = float.Parse(word.Substring(3, word.Length - 3), CultureInfo.InvariantCulture);
+
+                }
+            }
+        }
+    }
+}
---
+++ TestMyLib/BasicDataShareTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using MyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMyLib
+{
+    [TestFixture]
+    class BasicDataShareTest
+    {
+        [Test]
+        public void ParseBasicDataShare()
+        {
+            DateTime t1 = new DateTime(2014,11,12);
+            DateTime t2 = new DateTime(2014, 11, 14);
+            string data = "BDSh;i844;SiYKBNK.HE;s1;AVq50000;As20141112;Ae20141114;"; 
+            BasicDataShare BDSh = new BasicDataShare(data);
+            Assert.AreEqual(844,BDSh.i);
+            Assert.AreEqual("YKBNK.HE",BDSh.Si);
+            Assert.AreEqual(1,BDSh.s);
+            Assert.AreEqual(50000,BDSh.AVq);
+            Assert.AreEqual(t1,BDSh.As);
+            Assert.AreEqual(t2, BDSh.Ae);
+        }
+    }
+}
