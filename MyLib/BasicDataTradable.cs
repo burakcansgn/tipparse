2d1e33792ba42212d65c711a2ece0a7bfe586c3b
Burakcan <burakcansgn@gmail.com>
2016-11-28 15:39:31 +0200

    Basic Data Tradable

---
+++ MyLib/BasicDataTradable.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    public class BasicDataTradable
+    {
+        #region 37parametre
+        public readonly int i;
+        public readonly string Si;
+        public readonly int s;
+        public readonly int Mk;
+        public readonly string INi;
+        public readonly string SYm;
+        public readonly string NAm;
+        public readonly string SNm;
+        public readonly int ISi;
+        public readonly string CUi;
+        public readonly string CUt;
+        public readonly int PRt;
+        public readonly int VOd;
+        public readonly DateTime LDa;
+        public readonly DateTime TTd;
+        public readonly string IEt;
+        public readonly float NMv;
+        public readonly int ITSz;
+        public readonly int NDp;
+        public readonly int NDTp;
+        public readonly int CLId;
+        public readonly string CNy;
+        public readonly int SSc;
+        public readonly int STy;
+        public readonly Boolean AUm;
+        public readonly Boolean TRa;
+        public readonly Boolean PTa;
+        public readonly int PTb;
+        public readonly int MSe;
+        public readonly Boolean HOt;
+        public readonly float LSz;
+        public readonly Boolean SSv;
+        public readonly int MLm;
+        public readonly int MLt;
+        public readonly int TRId;
+        public readonly int MMk;
+        public readonly string TRm;
+        public readonly Boolean GRs;
+        private string[] checks = { "i", "Si", "s", "Mk", "INi", "SYm", "NAm", "SNm", "ISi", "CUi", "CUt", "PRt", "VOd", "LDa", "TTd", "IEt", "NMv", "ITSz", "NDp", "NDTp", "CLId", "CNy", "SSc", "STy", "AUm", "TRa", "PTa", "PTb", "MSe", "HOt", "LSz", "SSv", "MLm", "MLt", "TRId", "MMk", "TRm", "GRs" };
+        #endregion
+        public BasicDataTradable(string name)
+        {
+            string[] words = name.Split(';');
+            foreach (string word in words)
+            {
+                foreach (string check in checks)
+                {
+                    #region 37if
+                    if (word.StartsWith(checks[0]))
+                        i = Int32.Parse(word.Substring(1, word.Length - 1));
+                    else if (word.StartsWith(checks[1]))
+                        Si = word.Substring(2, word.Length - 2);
+                    else if (word.StartsWith(checks[2]))
+                        s = Int32.Parse(word.Substring(1, word.Length - 1));
+                    else if (word.StartsWith(checks[3]))
+                        Mk = Int32.Parse(word.Substring(2, word.Length - 2));
+                    else if (word.StartsWith(checks[4]))
+                        INi = word.Substring(3, word.Length - 3);
+                    else if (word.StartsWith(checks[5]))
+                        SYm = word.Substring(3, word.Length - 3);
+                    else if (word.StartsWith(checks[6]))
+                        NAm = word.Substring(3, word.Length - 3);
+                    else if (word.StartsWith(checks[7]))
+                        SNm = word.Substring(3, word.Length - 3);
+                    else if (word.StartsWith(checks[8]))
+                        ISi = Int32.Parse(word.Substring(3, word.Length - 3));
+                    else if (word.StartsWith(checks[9]))
+                        CUi = word.Substring(3, word.Length - 3);
+                    else if (word.StartsWith(checks[10]))
+                        CUt = word.Substring(3, word.Length - 3);
+                    else if (word.StartsWith(checks[11]))
+                        PRt = Int32.Parse(word.Substring(3, word.Length - 3));
+                    else if (word.StartsWith(checks[12]))
+                        VOd = Int32.Parse(word.Substring(3, word.Length - 3));
+                    else if (word.StartsWith(checks[13]))
+                        LDa = DateTime.ParseExact(word.Substring(3, word.Length - 3), "yyyyMMdd", CultureInfo.InvariantCulture);
+                    else if (word.StartsWith(checks[14]))
+                        LDa = DateTime.ParseExact(word.Substring(3, word.Length - 3), "yyyyMMdd", CultureInfo.InvariantCulture);
+                    else if (word.StartsWith(checks[15]))
+                        IEt = word.Substring(3, word.Length - 3);
+                    else if (word.StartsWith(checks[16]))
+                        NMv = float.Parse(word.Substring(3, word.Length - 3), CultureInfo.InvariantCulture);
+                    else if (word.StartsWith(checks[17]))
+                        ITSz = Int32.Parse(word.Substring(4, word.Length - 4));
+                    else if (word.StartsWith(checks[18]))
+                        NDp = Int32.Parse(word.Substring(3, word.Length - 3));
+                    else if (word.StartsWith(checks[19]))
+                        NDTp = Int32.Parse(word.Substring(4, word.Length - 4));
+                    else if (word.StartsWith(checks[20]))
+                        CLId = Int32.Parse(word.Substring(4, word.Length - 4));
+                    else if (word.StartsWith(checks[21]))
+                        CNy = word.Substring(3, word.Length - 3);
+                    else if (word.StartsWith(checks[22]))
+                        SSc = Int32.Parse(word.Substring(3, word.Length - 3));
+                    else if (word.StartsWith(checks[23]))
+                        STy = Int32.Parse(word.Substring(3, word.Length - 3));
+                    else if (word.StartsWith(checks[24]))
+                    {
+                        if (word.Substring(3, word.Length - 3) == "N")
+                            AUm = false;
+                        else
+                            AUm = true;
+                    }
+                    else if (word.StartsWith(checks[25]))
+                    {
+                        if (word.Substring(3, word.Length - 3) == "N")
+                            TRa = false;
+                        else
+                            TRa = true;
+                    }
+                    else if (word.StartsWith(checks[26]))
+                    {
+                        if (word.Substring(3, word.Length - 3) == "N")
+                            PTa = false;
+                        else
+                            PTa = true;
+                    }
+                    else if (word.StartsWith(checks[27]))
+                        PTb = Int32.Parse(word.Substring(3, word.Length - 3));
+                    else if (word.StartsWith(checks[28]))
+                        MSe = Int32.Parse(word.Substring(3, word.Length - 3));
+                    else if (word.StartsWith(checks[29]))
+                    {
+                        if (word.Substring(3, word.Length - 3) == "N")
+                            HOt = false;
+                        else
+                            HOt = true;
+                    }
+                    else if (word.StartsWith(checks[30]))
+                        LSz = float.Parse(word.Substring(3, word.Length - 3), CultureInfo.InvariantCulture);
+                    else if (word.StartsWith(checks[31]))
+                    {
+                        if (word.Substring(3, word.Length - 3) == "N")
+                            SSv = false;
+                        else
+                            SSv = true;
+                    }
+                    else if (word.StartsWith(checks[32]))
+                        MLm = Int32.Parse(word.Substring(3, word.Length - 3));
+                    else if (word.StartsWith(checks[33]))
+                        MLt = Int32.Parse(word.Substring(3, word.Length - 3));
+                    else if (word.StartsWith(checks[34]))
+                        TRId = Int32.Parse(word.Substring(4, word.Length - 4));
+                    else if (word.StartsWith(checks[35]))
+                        MMk = Int32.Parse(word.Substring(3, word.Length - 3));
+                    else if (word.StartsWith(checks[36]))
+                        TRm = word.Substring(3, word.Length - 3);
+                    else if (word.StartsWith(checks[37]))
+                    {
+                        if (word.Substring(3, word.Length - 3) == "N")
+                            GRs = false;
+                        else
+                            GRs = true;
+                    }
+#endregion
+                }
+            }
+        }
+    }
+}
---
+++ TestMyLib/BasicDataTradableTest.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using MyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMyLib
+{
+    [TestFixture]
+    class BasicDataTradableTest
+    {
+        [Test]
+        public void ParseBasicDataTradable()
+        {
+            DateTime t1 = new DateTime(2014, 04, 28);
+            DateTime t2 = new DateTime(2015, 09, 23);
+            string data = "BDt;i388;SiGOODY.E;s1;Mk288;INiMSPOTEQTGOODY;SYmGOODY.E;NAmGOODYEAR;ISi368;CUiTRY;CUtTRY;PRt1;VOd1;LDa20140428;ITSz78;NDp3;NDTp3;CNyTR;STy1;AUmY;TRaY;PTaY;PTb1;MSe56;LSz1;MLm1;MLt10000000;TRId100;TRmSM;GRsN;";
+            BasicDataTradable BDt = new BasicDataTradable(data);
+            Assert.AreEqual(388,BDt.i);
+            Assert.AreEqual("GOODY.E", BDt.Si);
+            Assert.AreEqual("GOODY.E", BDt.SYm);
+            Assert.AreEqual(1, BDt.s);
+            Assert.AreEqual(288, BDt.Mk);
+            Assert.AreEqual("MSPOTEQTGOODY", BDt.INi);
+            Assert.AreEqual("GOODYEAR", BDt.NAm);
+            Assert.AreEqual(368, BDt.ISi);
+            Assert.AreEqual("TRY", BDt.CUi);
+            Assert.AreEqual("TRY", BDt.CUt);
+            Assert.AreEqual(1, BDt.PRt);
+            Assert.AreEqual(1, BDt.VOd);
+            Assert.AreEqual(t1, BDt.LDa);
+            Assert.AreEqual(78, BDt.ITSz);
+            Assert.AreEqual(3, BDt.NDp);
+            Assert.AreEqual(3, BDt.NDTp);
+            Assert.AreEqual("TR", BDt.CNy);
+            Assert.AreEqual(1, BDt.STy);
+            Assert.AreEqual(true, BDt.AUm);
+            Assert.AreEqual(true, BDt.TRa);
+            Assert.AreEqual(true, BDt.PTa);
+            Assert.AreEqual(1, BDt.PTb);
+            Assert.AreEqual(56, BDt.MSe);
+            Assert.AreEqual(1, BDt.LSz);
+            Assert.AreEqual(1, BDt.MLm);
+            Assert.AreEqual(10000000,BDt.MLt);
+            Assert.AreEqual(100, BDt.TRId);
+            Assert.AreEqual("SM", BDt.TRm);
+            Assert.AreEqual(false, BDt.GRs);
+
+
+
+        }
+    }
+}
