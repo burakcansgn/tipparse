4dc2bbb33dfd5e4b3b362b5f1ec266191c1924e0
Burakcan <burakcansgn@gmail.com>
2016-12-05 14:10:59 +0200

    Basic Data Underlying Info

---
+++ MyLib/BasicDataUnderlyingInfo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    public class BasicDataUnderlyingInfo
+    {
+        public readonly int i;
+        public readonly string Si;
+        public readonly int s;
+        public readonly int ULi;
+        public readonly string DSc;
+        public readonly Boolean HOt;
+        string[] checks = { "i", "Si", "s", "ULi", "DSc", "Hot" };
+
+        public BasicDataUnderlyingInfo(string name)
+        {
+            string[] words = name.Split(';');
+            foreach (string word in words)
+            {
+                foreach (string check in checks)
+                {
+                    if (word.StartsWith(checks[0]))
+                        i = Int32.Parse(word.Substring(1, word.Length - 1));
+                    else if (word.StartsWith(checks[1]))
+                        Si = word.Substring(2, word.Length - 2);
+                    else if (word.StartsWith(checks[2]))
+                        s = Int32.Parse(word.Substring(1, word.Length - 1));
+                    else if (word.StartsWith(checks[3]))
+                        ULi = Int32.Parse(word.Substring(3, word.Length - 3));
+                    else if (word.StartsWith(checks[4]))
+                        DSc = word.Substring(3, word.Length - 3);
+                    else if (word.StartsWith(checks[5]))
+                    {
+                        if (word.Substring(3, word.Length - 3) == "N")
+                            HOt = false;
+                        else
+                            HOt = true;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ TestMyLib/BasicDataUnderlyingInfoTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using MyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMyLib
+{
+    [TestFixture]
+    class BasicDataUnderlyingInfoTest
+    {
+        [Test]
+        public void ParseBasicDataUnderlyingInfo()
+        {
+            string data = @"BDUi;i396;SiGARIYMEPW999.MV;s1;DScGARAN ISSUER*\:IYM;";
+            BasicDataUnderlyingInfo BDUi = new BasicDataUnderlyingInfo(data);
+            Assert.AreEqual(396,BDUi.i);
+            Assert.AreEqual("GARIYMEPW999.MV",BDUi.Si);
+            Assert.AreEqual(1,BDUi.s);
+            Assert.AreEqual(@"GARAN ISSUER*\:IYM",BDUi.DSc);
+            
+        }
+    }
+}
