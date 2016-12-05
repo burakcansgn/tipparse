6ea3630b443dafa554405f799a6faa9c329b1382
Burakcan <burakcansgn@gmail.com>
2016-12-05 16:30:32 +0200

    Basic Data List Member

---
+++ MyLib/BasicDataListMember.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    public class BasicDataListMember
+    {
+        public readonly int IDo;
+        public readonly int LSi;
+        public readonly int s;
+        public readonly Boolean HOt;
+        string[] checks = { "IDo", "LSi", "s", "HOt" };
+
+        public BasicDataListMember(string name)
+        {
+            string[] words = name.Split(';');
+            foreach (string word in words)
+            {
+                foreach (string check in checks)
+                {
+                    if (word.StartsWith(checks[0]))
+                        IDo = Int32.Parse(word.Substring(3, word.Length - 3));
+                    else if (word.StartsWith(checks[1]))
+                        LSi = Int32.Parse(word.Substring(3, word.Length - 3));
+                    else if (word.StartsWith(checks[2]))
+                        s = Int32.Parse(word.Substring(1, word.Length - 1));
+                    else if (word.StartsWith(checks[3]))
+                    {
+                        if (word.Substring(3, word.Length - 3) == "N")
+                            HOt = false;
+                        else
+                            HOt = true;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ TestMyLib/BasicDataListMemberTest.cs
@@ -0,0 +1,24 @@
+﻿using MyLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMyLib
+{
+    [TestFixture]
+    class BasicDataListMemberTest
+    {
+        [Test]
+        public void ParseBasicDataListMember()
+        {
+            string data = "BDLm;IDo410;LSi38;s1;";
+            BasicDataListMember BDLm = new BasicDataListMember(data);
+            Assert.AreEqual(410,BDLm.IDo);
+            Assert.AreEqual(38,BDLm.LSi);
+            Assert.AreEqual(1,BDLm.s);
+        }
+    }
+}
