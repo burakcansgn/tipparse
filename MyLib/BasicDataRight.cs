0db14f75e5b4cc22d3cd64ddb3338677d023b2ba
Burakcan <burakcansgn@gmail.com>
2016-12-05 13:52:09 +0200

    Basic Data Right

---
+++ MyLib/BasicDataRight.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    public class BasicDataRight
+    {
+        public readonly int i;
+        public readonly string Si;
+        public readonly int s;
+        public readonly float CSz;
+        public readonly DateTime EXb;
+        public readonly DateTime EXe;
+        public readonly float TIs;
+        public readonly int SEt;
+        public readonly string EXc;
+        public readonly Boolean HOt;
+        string[] checks = { "i", "Si", "s", "CSz", "EXb", "EXe", "TIs", "SEt", "EXc", "HOt" };
+
+        public BasicDataRight(string name)
+        {
+            string[] words = name.Split(';');
+            foreach (string word in words)
+            {
+                foreach (string check in checks)
+                {
+                    if (word.StartsWith(checks[0]))
+                        i = Int32.Parse(word.Substring(1, word.Length - 1));
+                    else if (word.StartsWith(checks[1]))
+                        Si = word.Substring(2, word.Length - 2);
+                    else if (word.StartsWith(checks[2]))
+                        s = Int32.Parse(word.Substring(1, word.Length - 1));
+                    else if (word.StartsWith(checks[3]))
+                        CSz = float.Parse(word.Substring(3, word.Length - 3), CultureInfo.InvariantCulture);
+                    else if (word.StartsWith(checks[4]))
+                        EXb = DateTime.ParseExact(word.Substring(3, word.Length - 3), "yyyyMMdd", CultureInfo.InvariantCulture);
+                    else if (word.StartsWith(checks[5]))
+                        EXe = DateTime.ParseExact(word.Substring(3, word.Length - 3), "yyyyMMdd", CultureInfo.InvariantCulture);
+                    else if (word.StartsWith(checks[6]))
+                        TIs = float.Parse(word.Substring(3, word.Length - 3), CultureInfo.InvariantCulture);
+                    else if (word.StartsWith(checks[7]))
+                        SEt = Int32.Parse(word.Substring(3, word.Length - 3));
+                    else if (word.StartsWith(checks[8]))
+                        EXc = word.Substring(3, word.Length - 3);
+                    else if (word.StartsWith(checks[9]))
+                    {
+                        if (word.Substring(3, word.Length - 3) == "N")
+                            HOt = false;
+                        else
+                            HOt = true;
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ TestMyLib/BasicDataRightTest.cs
@@ -0,0 +1,28 @@
+﻿using MyLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMyLib
+{
+    [TestFixture]
+    class BasicDataRightTest
+    {
+        [Test]
+        public void ParseBasicDataRight()
+        {
+            string data = "BDRi;i486;SiBURCE.R;s1;EXcTRY;";
+            BasicDataRight BDRi = new BasicDataRight(data);
+            Assert.AreEqual(486, BDRi.i);
+            Assert.AreEqual("BURCE.R", BDRi.Si);
+            Assert.AreEqual(1, BDRi.s);
+            Assert.AreEqual("TRY", BDRi.EXc);
+
+        }
+
+
+    }
+}
