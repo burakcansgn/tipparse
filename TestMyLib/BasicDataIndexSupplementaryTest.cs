78d1f549a138bc4acdea2ebe873b009f2cc20c16
Burakcan <burakcansgn@gmail.com>
2016-12-05 18:04:48 +0200

    Basic Data Index Suplementary

---
+++ TestMyLib/BasicDataIndexSupplementaryTest.cs
@@ -0,0 +1,25 @@
+﻿using MyLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMyLib
+{
+    [TestFixture]
+    class BasicDataIndexSupplementaryTest
+    {
+        [Test]
+        public void ParseBasicDataIndexSupplementary()
+        {
+            string data = "BDIp;i2548;SiXUTUM;s2;ISnTRAIMKB00044;";
+            BasicDataIndexSupplementary BDIp = new BasicDataIndexSupplementary(data);
+            Assert.AreEqual(2548, BDIp.i);
+            Assert.AreEqual("XUTUM", BDIp.Si);
+            Assert.AreEqual("TRAIMKB00044", BDIp.ISn);
+
+        }
+    }
+}
