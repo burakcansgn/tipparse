b189e00343f1fe257bbc4748d0c8578a0364f60d
Burakcan <burakcansgn@gmail.com>
2016-12-05 11:54:09 +0200

    Basic Data Fund

---
+++ TestMyLib/BasicDataFundTest.cs
@@ -0,0 +1,23 @@
+﻿using MyLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMyLib
+{   [TestFixture]
+    class BasicDataFundTest
+    {
+       [Test]
+       public void ParseBasicDataFund()
+        {
+            string data = "BDEt;i566;SiSLVRP.F;s1;";
+            BasicDataFund BDEt = new BasicDataFund(data);
+            Assert.AreEqual(566,BDEt.i);
+            Assert.AreEqual("SLVRP.F",BDEt.Si);
+            Assert.AreEqual(1,BDEt.s);
+        }
+    }
+}
