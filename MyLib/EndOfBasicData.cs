0ce758008a7e1378ecd5dacb0f8a30740222bd9b
Burakcan <burakcansgn@gmail.com>
2016-12-05 16:52:30 +0200

    End Of Basic Data

---
+++ MyLib/EndOfBasicData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+   public class EndOfBasicData
+    {
+        public readonly int s;
+        string[] checks = { "s" };
+
+        public EndOfBasicData(string name)
+        {
+            string[] words = name.Split(';');
+
+            foreach(string word in words)
+            {
+                foreach(string check in checks)
+                {
+                    if (word.StartsWith(checks[0]))
+                        s = Int32.Parse(word.Substring(1, word.Length - 1));
+                }
+            }
+
+        }
+    }
+}
---
+++ TestMyLib/EndOfBasicDataTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using MyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMyLib
+{
+    [TestFixture]
+    class EndOfBasicDataTest
+    {
+        [Test]
+        public void ParseEndOfBasicData()
+        {
+            string data = "EOBd;s1;";
+            EndOfBasicData EOBd = new EndOfBasicData(data);
+            Assert.AreEqual(1, EOBd.s);
+
+        }
+    }
+}
