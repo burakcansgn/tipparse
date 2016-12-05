3e1ba99a84240cc742cfd8c02019603dee7756ed
Burakcan <burakcansgn@gmail.com>
2016-12-05 11:34:36 +0200

    Basic Data Derivative

---
+++ MyLib/BasicDataDerivative.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    public class BasicDataDerivative
+    {
+        public readonly int i;
+        public readonly string Si;
+        public readonly int s;
+        public readonly int DTy;
+        public readonly float TIs;
+        public readonly int DXt;
+        public readonly float STp;
+        public readonly float CSz;
+        public readonly int SEt;
+        public readonly DateTime EXb;
+        public readonly DateTime EXe;
+        public readonly DateTime Sd;
+        public readonly Boolean HOt;
+        string[] checks = { "i", "Si", "s", "DTy", "TIs", "DXt", "STp", "CSz", "SEt", "EXb", "EXe", "Sd", "HOt" };
+
+        public BasicDataDerivative(string name)
+        {
+            string[] words = name.Split(';');
+            foreach (string word in words)
+            {
+                foreach (string check in checks)
+                {
+                    if (word.StartsWith(checks[0]))
+                        i = Int32.Parse(word.Substring(1, word.Length - 1));
+                    else if (word.StartsWith(checks[1]))
+                        Si = word.Substring(2, word.Length - 2);
+                    else if (word.StartsWith(checks[2]))
+                        s = Int32.Parse(word.Substring(1, word.Length - 1));
+                    else if (word.StartsWith(checks[3]))
+                        DTy = Int32.Parse(word.Substring(3, word.Length - 3));
+                    else if (word.StartsWith(checks[4]))
+                        TIs = float.Parse(word.Substring(3, word.Length - 3), CultureInfo.InvariantCulture);
+                    else if (word.StartsWith(checks[5]))
+                        DXt = Int32.Parse(word.Substring(3, word.Length - 3));
+                    else if (word.StartsWith(checks[6]))
+                        STp = float.Parse(word.Substring(3, word.Length - 3), CultureInfo.InvariantCulture);
+                    else if (word.StartsWith(checks[7]))
+                        CSz = float.Parse(word.Substring(3, word.Length - 3), CultureInfo.InvariantCulture);
+                    else if (word.StartsWith(checks[8]))
+                        SEt = Int32.Parse(word.Substring(3, word.Length - 3));
+                    else if (word.StartsWith(checks[9]))
+                        EXb = DateTime.ParseExact(word.Substring(3, word.Length - 3), "yyyyMMdd", CultureInfo.InvariantCulture);
+                    else if (word.StartsWith(checks[10]))
+                        EXe = DateTime.ParseExact(word.Substring(3, word.Length - 3), "yyyyMMdd", CultureInfo.InvariantCulture);
+                    else if (word.StartsWith(checks[11]))
+                       Sd = DateTime.ParseExact(word.Substring(2, word.Length - 2), "yyyyMMdd", CultureInfo.InvariantCulture);
+                    else if (word.StartsWith(checks[12]))
+                    {
+                        if (word.Substring(3, word.Length - 3) == "N")
+                            HOt = false;
+                        else
+                            HOt = true;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ TestMyLib/BasicDataDerivativeTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using MyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMyLib
+{
+    [TestFixture]
+    class BasicDataDerivativeTest
+    {
+        [Test]
+       public void ParseBasicDataDerivative()
+        {
+            DateTime d1 = new DateTime();
+            DateTime d2 = new DateTime(2064,07,14);
+            DateTime d3 = new DateTime();
+            string data = "BDDe;i518;SiGARDBLSCR555;s1;DTy2;DXt4;CSz1;SEt1;EXe20640714;";
+            BasicDataDerivative BDDe = new BasicDataDerivative(data);
+            Assert.AreEqual(518,BDDe.i);
+            Assert.AreEqual("GARDBLSCR555",BDDe.Si);
+            Assert.AreEqual(1,BDDe.s);
+            Assert.AreEqual(2,BDDe.DTy);
+            Assert.AreEqual(4,BDDe.DXt);
+            Assert.AreEqual(1,BDDe.CSz);
+            Assert.AreEqual(1,BDDe.SEt);
+            Assert.AreEqual(d2,BDDe.EXe);
+        }
+    }
+}
