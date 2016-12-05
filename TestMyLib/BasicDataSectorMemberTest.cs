7d1fc2e95d9da5bd696223da54bf166af1c264ca
Burakcan <burakcansgn@gmail.com>
2016-12-05 16:43:01 +0200

    Basic Data Sector Member

---
+++ TestMyLib/BasicDataSectorMemberTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using MyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMyLib
+{
+    [TestFixture]
+    class BasicDataSectorMemberTest
+    {
+        [Test]
+        public void ParseBasicDataSectorMember()
+        {
+            string data = "BDSm;IDo1288;SId128;s1;";
+            BasicDataSectorMember BDSm = new BasicDataSectorMember(data);
+            Assert.AreEqual(1288,BDSm.IDo);
+            Assert.AreEqual(128,BDSm.SId);
+            Assert.AreEqual(1,BDSm.s);
+        }
+    }
+}
