e5a6cef6915977478e8804a3a162338b149ca37d
Burakcan <burakcansgn@gmail.com>
2016-12-09 10:29:02 +0200

    Data Index Member

---
+++ MyLib/BasicDataIndexMember.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    public class BasicDataIndexMember
+    {
+        public readonly int IDo;
+        public readonly int IXi;
+        public readonly int s;
+        string[] checks = { "IDo", "IXi", "s" };
+
+        public BasicDataIndexMember(string name)
+        {
+            string[] words = name.Split(';');
+            foreach (string word in words)
+            {
+                foreach (string check in checks)
+                {
+                    if (word.StartsWith(checks[0]))
+                        IDo = Int32.Parse(word.Substring(3, word.Length - 3));
+                    else if (word.StartsWith(checks[1]))
+                        IXi = Int32.Parse(word.Substring(3, word.Length - 3));
+                    else if (word.StartsWith(checks[2]))
+                        s = Int32.Parse(word.Substring(1, word.Length - 1));
+                }
+            }
+        }
+    }
+}
---
+++ TestMyLib/BasicDataIndexMemberTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using MyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMyLib
+{
+    [TestFixture]
+    class BasicDataIndexMemberTest
+    {
+        [Test]
+        public void ParseBasicDataIndexMember()
+        {
+            string data = "BDIm;IDo388;IXi2548;s2;";
+            BasicDataIndexMember BDIm = new BasicDataIndexMember(data);
+            Assert.AreEqual(388,BDIm.IDo);
+            Assert.AreEqual(2548,BDIm.IXi);
+            Assert.AreEqual(2,BDIm.s);
+        }
+    }
+}
