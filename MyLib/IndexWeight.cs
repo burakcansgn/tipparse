e628841a2faecf0c8d23443890c875cb25ad4ccd
Burakcan <burakcansgn@gmail.com>
2016-12-09 14:50:34 +0200

    Index Weight

---
+++ MyLib/IndexWeight.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+
+    public class IndexWeight
+    {
+        public readonly int i;
+        public readonly int s;
+        public readonly TimeSpan t;
+        public readonly int IDo;
+        public readonly decimal WPe;
+        public readonly decimal FFr;
+        public readonly decimal Wf;
+        public readonly decimal WFv;
+        public readonly decimal CAp;
+        public readonly SEd Sed;
+        string[] checks = { "i", "s", "t", "IDo", "WPe", "FFr", "Wf", "WFv", "CAp", "SEd" };
+
+        public IndexWeight(string name)
+        {
+            string[] words = name.Split(';');
+            foreach (string word in words)
+            {
+                foreach (string check in checks)
+                {
+                    if (word.StartsWith(checks[0]))
+                        i = Int32.Parse(word.Substring(1, word.Length - 1));
+                    else if (word.StartsWith(checks[1]))
+                        s = Int32.Parse(word.Substring(1, word.Length - 1));
+                    else if (word.StartsWith(checks[2]))
+                        t = TimeSpan.ParseExact(word.Substring(1, word.Length - 1), "hhmmss\\.fff", CultureInfo.InvariantCulture);
+                    else if (word.StartsWith(checks[3]))
+                        IDo = Int32.Parse(word.Substring(3, word.Length - 3));
+                    else if (word.StartsWith(checks[4]))
+                        WPe = decimal.Parse(word.Substring(3, word.Length - 3), CultureInfo.InvariantCulture);
+                    else if (word.StartsWith(checks[5]))
+                        FFr = decimal.Parse(word.Substring(3, word.Length - 3), CultureInfo.InvariantCulture);
+                    else if (word.StartsWith(checks[6]))
+                        Wf = decimal.Parse(word.Substring(2, word.Length - 2), CultureInfo.InvariantCulture);
+                    else if (word.StartsWith(checks[7]))
+                        WFv = decimal.Parse(word.Substring(3, word.Length - 3), CultureInfo.InvariantCulture);
+                    else if (word.StartsWith(checks[8]))
+                        CAp = decimal.Parse(word.Substring(3, word.Length - 3), CultureInfo.InvariantCulture);
+                    else if (word.StartsWith(checks[9]))
+                    {
+                        if (word.Substring(3, word.Length - 3) == "1")
+                            Sed = SEd.StartofDay;
+                        else if (word.Substring(3, word.Length - 3) == "2")
+                            Sed = SEd.EndofDay;
+                        else
+                            Sed = SEd.None;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ TestMyLib/IndexWeightTest.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using MyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMyLib
+{
+    [TestFixture]
+    class IndexWeightTest
+    {
+        [Test]
+        public void ParseIndexWeight()
+        {
+            SEd mysed = SEd.StartofDay;
+            TimeSpan ts = new TimeSpan(0, 04, 10, 00, 000);
+            string data = "Iw;i2550;s2;t041000.000;IDo1024;WPe0.224202;FFr0.44;Wf1;WFv341510400;CAp776160000;SEd1;";
+            IndexWeight Iw = new IndexWeight(data);
+            Assert.AreEqual(2550,Iw.i);
+            Assert.AreEqual(2,Iw.s);
+            Assert.AreEqual(ts,Iw.t);
+            Assert.AreEqual(1024,Iw.IDo);
+            Assert.AreEqual(0.224202,Iw.WPe);
+            Assert.AreEqual(0.44,Iw.FFr);
+            Assert.AreEqual(1,Iw.Wf);
+            Assert.AreEqual(341510400,Iw.WFv);
+            Assert.AreEqual(776160000,Iw.CAp);
+            Assert.AreEqual(mysed,Iw.Sed);
+
+
+        }
+    }
+}
