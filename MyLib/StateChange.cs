9c4528db9ac8d2431c61436aafcbaeec71c13d58
Burakcan <burakcansgn@gmail.com>
2016-12-09 16:06:34 +0200

    State Change

---
+++ MyLib/StateChange.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+   public class StateChange
+    {
+        public readonly int i;
+        public readonly int s;
+        public readonly TimeSpan t;
+        public readonly int Ms;
+        public readonly int Sl;
+        public readonly Boolean Of;
+        string[] checks = { "i", "s", "t", "Ms", "Sl", "Of" };
+
+        public StateChange(string name)
+        {
+            string[] words = name.Split(';');
+            foreach (string word in words)
+            {
+                foreach (string check in checks)
+                {
+                    if (word.StartsWith(checks[0]))
+                        i = Int32.Parse(word.Substring(1, word.Length - 1));
+                    else if (word.StartsWith(checks[1]))
+                        s = Int32.Parse(word.Substring(1, word.Length - 1));
+                    else if (word.StartsWith(checks[2]))
+                        t = TimeSpan.ParseExact(word.Substring(1, word.Length - 1), "hhmmss\\.fff", CultureInfo.InvariantCulture);
+                    else if (word.StartsWith(checks[3]))
+                        Ms = Int32.Parse(word.Substring(2, word.Length - 2));
+                    else if (word.StartsWith(checks[4]))
+                        Sl = Int32.Parse(word.Substring(2, word.Length - 2));
+                    else if (word.StartsWith(checks[5]))
+                    {
+                        if (word.Substring(2, word.Length - 2) == "N")
+                            Of = false;
+                        else
+                            Of = true;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ TestMyLib/StateChangeTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using MyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMyLib
+{
+    [TestFixture]
+    class StateChangeTest
+    {
+        [Test]
+        public void ParseStateChange()
+        {
+            TimeSpan t = new TimeSpan(0, 07, 02, 02);
+            string data = "s;i260;s1;t070202.938;Ms99;Sl1;";
+            StateChange s = new StateChange(data);
+            Assert.AreEqual(260,s.i);
+            Assert.AreEqual(1,s.s);
+            Assert.AreEqual(t,s.t);
+            Assert.AreEqual(1,s.Sl);
+
+        }
+    }
+}
