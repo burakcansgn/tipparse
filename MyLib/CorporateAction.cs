335acb75b5ef602b974e7d3796b89642a941dfeb
Burakcan <burakcansgn@gmail.com>
2016-12-09 15:10:02 +0200

    Corporate Action

---
+++ MyLib/CorporateAction.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    public class CorporateAction
+    {
+        public readonly int i;
+        public readonly int s;
+        public readonly int SRe;
+        public readonly int NOc;
+        public readonly TimeSpan t;
+        string[] checks = { "i", "s", "SRe", "NOc","t" };
+
+        public CorporateAction(string name)
+        {
+            string[] words = name.Split(';');
+            foreach (string word in words)
+            {
+                foreach (string check in checks)
+                {
+                    if (word.StartsWith(checks[0]))
+                        i = Int32.Parse(word.Substring(1, word.Length - 1));
+                    else if (word.StartsWith(checks[1]))
+                        s = Int32.Parse(word.Substring(1, word.Length - 1));
+                    else if (word.StartsWith(checks[2]))
+                        SRe = Int32.Parse(word.Substring(3, word.Length - 3));
+                    else if (word.StartsWith(checks[3]))
+                        NOc = Int32.Parse(word.Substring(3, word.Length - 3));
+                    else if (word.StartsWith(checks[4]))
+                        t = TimeSpan.ParseExact(word.Substring(1, word.Length - 1), "hhmmss\\.fff", CultureInfo.InvariantCulture);
+                }
+            }
+        }
+    }
+}
---
+++ TestMyLib/CorporateActionTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using MyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMyLib
+{
+    [TestFixture]
+    class CorporateActionTest
+    {
+        [Test]
+        public void ParseCorparateAction()
+        {
+            TimeSpan ts = new TimeSpan(0,07,02,02,881);
+            string data = "TRh;i1288;s1;SRe1;NOc14;t070202.881;";
+            CorporateAction TRh = new CorporateAction(data);
+            Assert.AreEqual(1288,TRh.i);
+            Assert.AreEqual(1,TRh.s);
+            Assert.AreEqual(1,TRh.SRe);
+            Assert.AreEqual(14,TRh.NOc);
+            Assert.AreEqual(ts,TRh.t);
+        }
+    }
+}
