5e12a9665b8cded727a0578aa63f587f205c240c
Burakcan <burakcansgn@gmail.com>
2016-12-12 14:56:15 +0200

    Call Information1

---
+++ MyLib/CallInformation1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    public class CallInformation1
+    {
+        public readonly int i;
+        public readonly int s;
+        public readonly TimeSpan t;
+        public readonly decimal EQp;
+        public readonly decimal EQv;
+        string[] checks = { "i", "s", "t", "EQp", "EQv" };
+
+        public CallInformation1(string data)
+        {
+            string[] words = data.Split(';');
+            for (int m = 1; m < words.Length; m++)
+            {
+                if (words[m].StartsWith(checks[0]))
+                    i = Int32.Parse(words[m].Substring(1, words[m].Length - 1));
+                else if (words[m].StartsWith(checks[1]))
+                    s = Int32.Parse(words[m].Substring(1, words[m].Length - 1));
+                else if (words[m].StartsWith(checks[2]))
+                    t = TimeSpan.ParseExact(words[m].Substring(1, words[m].Length - 1), "hhmmss\\.fff", CultureInfo.InvariantCulture);
+                else if (words[m].StartsWith(checks[3]))
+                    EQp = decimal.Parse(words[m].Substring(3, words[m].Length - 3), CultureInfo.InvariantCulture);
+                else if (words[m].StartsWith(checks[4]))
+                    EQv = decimal.Parse(words[m].Substring(3, words[m].Length - 3), CultureInfo.InvariantCulture);
+            }
+        }
+    }
+}
---
+++ TestMyLib/CallInformation1Test.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using MyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMyLib
+{
+    [TestFixture]
+    class CallInformation1Test
+    {
+        [Test]
+        public void ParseCallInformation1()
+        {
+            TimeSpan t = new TimeSpan(0, 09, 17, 01, 680);
+            string data = "c;i950;s1;t091701.680;EQp9.1;EQv36250;";
+            CallInformation1 c = new CallInformation1(data);
+            Assert.AreEqual(950,c.i);
+            Assert.AreEqual(1,c.s);
+            Assert.AreEqual(t,c.t);
+            Assert.AreEqual(9.1,c.EQp);
+            Assert.AreEqual(36250,c.EQv);
+        }
+    }
+}
