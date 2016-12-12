52977a4676adbb2f9eb8a571efc825f96fbd6676
Burakcan <burakcansgn@gmail.com>
2016-12-12 14:35:38 +0200

    Trade Statistics3

---
+++ MyLib/TradeStatistics3.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    public class TradeStatistics3
+    {
+        public readonly int i;
+        public readonly int s;
+        public readonly TimeSpan t;
+        public readonly decimal Pf;
+        public readonly decimal Pl;
+        public readonly decimal Ph;
+        public readonly decimal LOp;
+        public readonly decimal Pd;
+        public readonly int q;
+        public readonly decimal o;
+        public readonly decimal Rq;
+        public readonly decimal f;
+        public readonly decimal Rt;
+        public readonly decimal LTRp;
+        public readonly int LTRq;
+        public readonly Boolean Of;
+        string[] checks = { "i", "s", "t", "Pf", "Pl", "Ph", "LOp", "Pd", "q", "o", "Rq", "f", "Rt", "LTRp", "LTRq", "Of" };
+
+        public TradeStatistics3(string data)
+        {
+            string[] words = data.Split(';');
+            for (int m = 1; m < words.Length; m++)
+            {
+                if (words[m].StartsWith(checks[0]))
+                    i = Int32.Parse(words[m].Substring(1, words[m].Length - 1));
+                else if (words[m].StartsWith(checks[1]))
+                    s = Int32.Parse(words[m].Substring(1, words[m].Length - 1));
+                else if (words[m].StartsWith(checks[2]))
+                    t = TimeSpan.ParseExact(words[m].Substring(1, words[m].Length - 1), "hhmmss\\.fff", CultureInfo.InvariantCulture);
+                else if (words[m].StartsWith(checks[3]))
+                    Pf = decimal.Parse(words[m].Substring(2, words[m].Length - 2), CultureInfo.InvariantCulture);
+                else if (words[m].StartsWith(checks[4]))
+                    Pl = decimal.Parse(words[m].Substring(2, words[m].Length - 2), CultureInfo.InvariantCulture);
+                else if (words[m].StartsWith(checks[5]))
+                    Ph = decimal.Parse(words[m].Substring(2, words[m].Length - 2), CultureInfo.InvariantCulture);
+                else if (words[m].StartsWith(checks[6]))
+                    LOp = decimal.Parse(words[m].Substring(2, words[m].Length - 2), CultureInfo.InvariantCulture);
+                else if (words[m].StartsWith(checks[7]))
+                    Pd = decimal.Parse(words[m].Substring(2, words[m].Length - 2), CultureInfo.InvariantCulture);
+                else if (words[m].StartsWith(checks[8]))
+                    q = Int32.Parse(words[m].Substring(1, words[m].Length - 1));
+                else if (words[m].StartsWith(checks[9]))
+                    o = decimal.Parse(words[m].Substring(1, words[m].Length - 1), CultureInfo.InvariantCulture);
+                else if (words[m].StartsWith(checks[10]))
+                    Rq = decimal.Parse(words[m].Substring(2, words[m].Length - 2), CultureInfo.InvariantCulture);
+                else if (words[m].StartsWith(checks[11]))
+                    f = decimal.Parse(words[m].Substring(1, words[m].Length - 1), CultureInfo.InvariantCulture);
+                else if (words[m].StartsWith(checks[12]))
+                    Rt = decimal.Parse(words[m].Substring(2, words[m].Length - 2), CultureInfo.InvariantCulture);
+                else if (words[m].StartsWith(checks[13]))
+                    LTRp = decimal.Parse(words[m].Substring(4, words[m].Length - 4), CultureInfo.InvariantCulture);
+                else if (words[m].StartsWith(checks[14]))
+                    LTRq = Int32.Parse(words[m].Substring(4, words[m].Length - 4));
+                else if (words[m].StartsWith(checks[15]))
+                {
+                    if (words[m].Substring(2, words[m].Length - 2) == "Y")
+                        Of = true;
+                    else
+                        Of = false;
+                }
+            }
+        }
+    }
+}
---
+++ TestMyLib/TradeStatistics3Test.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using MyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMyLib
+{
+    [TestFixture]
+    class TradeStatistics3Test
+    {
+        [Test]
+        public void ParseTradeStatistics3()
+        {
+            TimeSpan t = new TimeSpan(0, 07, 02, 05, 164);
+            string data = "w;i388;s1;t070205.164;Of;";
+            TradeStatistics3 w = new TradeStatistics3(data);
+            Assert.AreEqual(388, w.i);
+            Assert.AreEqual(1, w.s);
+            Assert.AreEqual(t, w.t);
+            Assert.AreEqual(false, w.Of);
+        }
+    }
+}
