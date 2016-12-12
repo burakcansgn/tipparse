cd49fc6b8168dcface4b1e991abfd6f4f4c12807
Burakcan <burakcansgn@gmail.com>
2016-12-12 11:22:34 +0200

    Orderbook2

---
+++ TestMyLib/Orderbook2Test.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using MyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMyLib
+{
+    [TestFixture]
+    class Orderbook2Test
+    {
+        [Test]
+        public void ParseOrderbook2()
+        {
+            TimeSpan t = new TimeSpan(0, 07, 02, 04, 583);
+            string data = "o;i388;s1;t070204.583;Of;";
+            Orderbook1 o = new Orderbook1(data);
+            Assert.AreEqual(388, o.i);
+            Assert.AreEqual(1, o.s);
+            Assert.AreEqual(t, o.t);
+            Assert.AreEqual(false, o.Of);
+        }
+    }
+}
