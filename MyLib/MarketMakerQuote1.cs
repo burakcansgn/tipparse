d0a6b55bafa9427c97caea1bc143cca39a5b384b
Burakcan <burakcansgn@gmail.com>
2016-12-23 17:03:04 +0200

    Market Maker Quote1

---
+++ MyLib/MarketMakerQuote1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    public class MarketMakerQuote1
+    {
+        public readonly int i;
+        public readonly int s;
+        public readonly TimeSpan t;
+        public readonly decimal Pa;
+        public readonly decimal Pb;
+        string[] checks = { "i", "s", "t", "Pa", "Pb" };
+
+        public MarketMakerQuote1(string data)
+        {
+            string[] words = data.Split(';');
+            for (int m = 1; m < words.Length; m++)
+            {
+                if (words[m].StartsWith(checks[0]))
+                    i = Int32.Parse(words[m].Substring(1, words[m].Length - 1));
+                else if (words[m].StartsWith(checks[1]))
+                    s = Int32.Parse(words[m].Substring(1, words[m].Length - 1));
+                else if (words[m].StartsWith(checks[2]))
+                    t = TimeSpan.ParseExact(words[m].Substring(1, words[m].Length - 1), "hhmmss\\.fff", CultureInfo.InvariantCulture);
+                else if (words[m].StartsWith(checks[3]))
+                    Pa = decimal.Parse(words[m].Substring(2, words[m].Length - 2), CultureInfo.InvariantCulture);
+                else if (words[m].StartsWith(checks[4]))
+                    Pb = decimal.Parse(words[m].Substring(2, words[m].Length - 2), CultureInfo.InvariantCulture);
+            }
+        }
+    }
+}
---
+++ TestMyLib/MarketMakerQuote1Test.cs
@@ -0,0 +1,27 @@
+﻿using MyLib;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMyLib
+{
+    [TestFixture]
+    class MarketMakerQuote1Test
+    {
+        [Test]
+        public void ParseMarketMakerQuote1()
+        {
+            TimeSpan t = new TimeSpan(0, 12, 05, 33, 162);
+            string data = "q;i1706;s1;t120533.162;Pa2.75;Pb2.7;";
+            MarketMakerQuote1 q = new MarketMakerQuote1(data);
+            Assert.AreEqual(1706, q.i);
+            Assert.AreEqual(1, q.s);
+            Assert.AreEqual(t, q.t);
+            Assert.AreEqual(2.75, q.Pa);
+            Assert.AreEqual(2.7, q.Pb);
+        }
+    }
+}
