bb9b0ea2ce495153ce503cf8f77ac77e4570e8ff
Burakcan <burakcansgn@gmail.com>
2016-12-23 16:22:21 +0200

    Index Update

---
+++ MyLib/IndexUpdate.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    public class IndexUpdate
+    {
+        public readonly int i;
+        public readonly int s;
+        public readonly TimeSpan t;
+        public readonly decimal Vc;
+        public readonly decimal Vh;
+        public readonly decimal Vl;
+        public readonly decimal o;
+        public readonly decimal f;
+        public readonly decimal OVa;
+        public readonly decimal Dn;
+        public readonly decimal Dd;
+        string[] checks = { "i", "s", "t", "Vc", "Vh", "Vl", "o", "f", "OVa", "Dn", "Dd" };
+
+        public IndexUpdate(string data)
+        {
+            string[] words = data.Split(';');
+            for (int m = 1; m < words.Length; m++)
+            {
+                if (words[m].StartsWith(checks[0]))
+                    i = Int32.Parse(words[m].Substring(1, words[m].Length - 1));
+                else if (words[m].StartsWith(checks[1]))
+                    s = Int32.Parse(words[m].Substring(1, words[m].Length - 1));
+                else if (words[m].StartsWith(checks[2]))
+                    t = TimeSpan.ParseExact(words[m].Substring(1, words[m].Length - 1), "hhmmss\\.fff", CultureInfo.InvariantCulture);
+                else if (words[m].StartsWith(checks[3]))
+                    Vc = decimal.Parse(words[m].Substring(2, words[m].Length - 2), CultureInfo.InvariantCulture);
+                else if (words[m].StartsWith(checks[4]))
+                    Vh = decimal.Parse(words[m].Substring(2, words[m].Length - 2), CultureInfo.InvariantCulture);
+                else if (words[m].StartsWith(checks[5]))
+                    Vl = decimal.Parse(words[m].Substring(2, words[m].Length - 2), CultureInfo.InvariantCulture);
+                else if (words[m].StartsWith(checks[6]))
+                    o = decimal.Parse(words[m].Substring(1, words[m].Length - 1), CultureInfo.InvariantCulture);
+                else if (words[m].StartsWith(checks[7]))
+                    f = decimal.Parse(words[m].Substring(1, words[m].Length - 1), CultureInfo.InvariantCulture);
+                else if (words[m].StartsWith(checks[8]))
+                    OVa = decimal.Parse(words[m].Substring(3, words[m].Length - 3), CultureInfo.InvariantCulture);
+                else if (words[m].StartsWith(checks[9]))
+                    Dn = decimal.Parse(words[m].Substring(2, words[m].Length - 2), CultureInfo.InvariantCulture);
+                else if (words[m].StartsWith(checks[10]))
+                    Dd = decimal.Parse(words[m].Substring(2, words[m].Length - 2), CultureInfo.InvariantCulture);
+            }
+        }
+    }
+}
+
---
+++ TestMyLib/IndexUpdateTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using MyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMyLib
+{
+    [TestFixture]
+    class IndexUpdateTest
+    {
+        [Test]
+        public void ParseIndexUpdate()
+        {
+            TimeSpan t = new TimeSpan(0, 09, 35, 00, 000);
+            string data = "i;i2600;s2;t093500.000;Vc99387.7509;Vh99387.7509;Vl99387.7509;o0;f0;OVa99387.7509;Dn0;Dd0;";
+            IndexUpdate i = new IndexUpdate(data);
+            Assert.AreEqual(2600,i.i);
+            Assert.AreEqual(2,i.s);
+            Assert.AreEqual(t,i.t);
+            Assert.AreEqual(99387.7509,i.Vc);
+            Assert.AreEqual(99387.7509,i.Vh);
+            Assert.AreEqual(99387.7509,i.Vl);
+            Assert.AreEqual(0,i.o);
+            Assert.AreEqual(0,i.f);
+            Assert.AreEqual(99387.7509,i.OVa);
+            Assert.AreEqual(0,i.Dn);
+            Assert.AreEqual(0, i.Dd);
+        }
+    }
+}
