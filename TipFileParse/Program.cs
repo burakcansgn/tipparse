ef2ede9470aad3a74c9330e4cc97e77978ff32c1
Burakcan <burakcansgn@gmail.com>
2016-12-29 10:10:06 +0200

    Refactor

--- TipFileParse/Program.cs
+++ TipFileParse/Program.cs
@@ -14,206 +14,206 @@ namespace TipFileParse
         static void Main(string[] args)
         {
             string text = System.IO.File.ReadAllText("tip_20150730_all.log");
-            string[] items = text.Split('\n');
-            for (int i = 0; i < items.Length; i++)
+            string[] tipMessages = text.Split('\n');
+            for (int i = 0; i < tipMessages.Length; i++)
             {
-                string[] messagefields = items[i].Split(';');
+                string[] messagefields = tipMessages[i].Split(';');
                 if (messagefields[0] == "BDBu")
                 {
-                    BasicDataBusinessDate bdbu = new BasicDataBusinessDate(items[i]);
+                    BasicDataBusinessDate bdbu = new BasicDataBusinessDate(tipMessages[i]);
                     Console.WriteLine(bdbu.ToString());
                 }
                 else if (messagefields[0] == "BDSr")
                 {
-                    BasicDataSource bdsr = new BasicDataSource(items[i]);
+                    BasicDataSource bdsr = new BasicDataSource(tipMessages[i]);
                 }
                 else if (messagefields[0] == "BDTe")
                 {
-                    BasicDataTableEntry bdte = new BasicDataTableEntry(items[i]);
+                    BasicDataTableEntry bdte = new BasicDataTableEntry(tipMessages[i]);
                 }
                 else if (messagefields[0] == "BDTs")
                 {
-                    BasicDataTickSizeTable bdts = new BasicDataTickSizeTable(items[i]);
+                    BasicDataTickSizeTable bdts = new BasicDataTickSizeTable(tipMessages[i]);
                 }
                 else if (messagefields[0] == "BDTz")
                 {
-                    BasicDataTickSizeEntry bdtz = new BasicDataTickSizeEntry(items[i]);
+                    BasicDataTickSizeEntry bdtz = new BasicDataTickSizeEntry(tipMessages[i]);
                 }
                 else if (messagefields[0] == "BDp")
                 {
-                    BasicDataParticipant bdp = new BasicDataParticipant(items[i]);
+                    BasicDataParticipant bdp = new BasicDataParticipant(tipMessages[i]);
                 }
                 else if (messagefields[0] == "BDCv")
                 {
-                    BasicDataClearingVenue bdcv = new BasicDataClearingVenue(items[i]);
+                    BasicDataClearingVenue bdcv = new BasicDataClearingVenue(tipMessages[i]);
                 }
                 else if (messagefields[0] == "BDx")
                 {
-                    BasicDataExchange bdx = new BasicDataExchange(items[i]);
+                    BasicDataExchange bdx = new BasicDataExchange(tipMessages[i]);
                 }
                 else if (messagefields[0] == "BDm")
                 {
-                    BasicDataMarket bdm = new BasicDataMarket(items[i]);
+                    BasicDataMarket bdm = new BasicDataMarket(tipMessages[i]);
                 }
                 else if (messagefields[0] == "BDTd")
                 {
-                    BasicDataNonTradingDays bdtd = new BasicDataNonTradingDays(items[i]);
+                    BasicDataNonTradingDays bdtd = new BasicDataNonTradingDays(tipMessages[i]);
                 }
                 else if (messagefields[0] == "BDTm")
                 {
-                    BasicDataTradingScheme bdtm = new BasicDataTradingScheme(items[i]);
+                    BasicDataTradingScheme bdtm = new BasicDataTradingScheme(tipMessages[i]);
                 }
                 else if (messagefields[0] == "BDLi")
                 {
-                    BasicDataList bdli = new BasicDataList(items[i]);
+                    BasicDataList bdli = new BasicDataList(tipMessages[i]);
                 }
                 else if (messagefields[0] == "BDs")
                 {
-                    BasicDataSector bds = new BasicDataSector(items[i]);
+                    BasicDataSector bds = new BasicDataSector(tipMessages[i]);
                 }
                 else if (messagefields[0] == "BDIs")
                 {
-                    BasicDataIssuer bdis = new BasicDataIssuer(items[i]);
+                    BasicDataIssuer bdis = new BasicDataIssuer(tipMessages[i]);
                 }
                 else if (messagefields[0] == "BDt")
                 {
-                    BasicDataTradable bdt = new BasicDataTradable(items[i]);
+                    BasicDataTradable bdt = new BasicDataTradable(tipMessages[i]);
                 }
                 else if (messagefields[0] == "BDTr")
                 {
-                    BasicDataTradableSupplementary bdtr = new BasicDataTradableSupplementary(items[i]);
+                    BasicDataTradableSupplementary bdtr = new BasicDataTradableSupplementary(tipMessages[i]);
                 }
                 else if (messagefields[0] == "BDSh")
                 {
-                    BasicDataShare bdsh = new BasicDataShare(items[i]);
+                    BasicDataShare bdsh = new BasicDataShare(tipMessages[i]);
                 }
                 else if (messagefields[0] == "BDDe")
                 {
-                    BasicDataDerivative bdde = new BasicDataDerivative(items[i]);
+                    BasicDataDerivative bdde = new BasicDataDerivative(tipMessages[i]);
                 }
                 else if (messagefields[0] == "BDEt")
                 {
-                    BasicDataFund bdet = new BasicDataFund(items[i]);
+                    BasicDataFund bdet = new BasicDataFund(tipMessages[i]);
                 }
                 else if (messagefields[0] == "BDRi")
                 {
-                    BasicDataRight bdri = new BasicDataRight(items[i]);
+                    BasicDataRight bdri = new BasicDataRight(tipMessages[i]);
                 }
                 else if (messagefields[0] == "BDUi")
                 {
-                    BasicDataUnderlyingInfo bdui = new BasicDataUnderlyingInfo(items[i]);
+                    BasicDataUnderlyingInfo bdui = new BasicDataUnderlyingInfo(tipMessages[i]);
                 }
                 else if (messagefields[0] == "m")
                 {
-                    OrderbookSummary m = new OrderbookSummary(items[i]);
+                    OrderbookSummary m = new OrderbookSummary(tipMessages[i]);
                 }
                 else if (messagefields[0] == "BDLm")
                 {
-                    BasicDataListMember bdlm = new BasicDataListMember(items[i]);
+                    BasicDataListMember bdlm = new BasicDataListMember(tipMessages[i]);
                 }
                 else if (messagefields[0] == "BDSm")
                 {
-                    BasicDataSectorMember bdsm = new BasicDataSectorMember(items[i]);
+                    BasicDataSectorMember bdsm = new BasicDataSectorMember(tipMessages[i]);
                 }
                 else if (messagefields[0] == "EOBd")
                 {
-                    EndOfBasicData eobd = new EndOfBasicData(items[i]);
+                    EndOfBasicData eobd = new EndOfBasicData(tipMessages[i]);
                 }
                 else if (messagefields[0] == "BDIn")
                 {
-                    BasicDataIndex bdin = new BasicDataIndex(items[i]);
+                    BasicDataIndex bdin = new BasicDataIndex(tipMessages[i]);
                 }
                 else if (messagefields[0] == "BDIp")
                 {
-                    BasicDataIndexSupplementary bdip = new BasicDataIndexSupplementary(items[i]);
+                    BasicDataIndexSupplementary bdip = new BasicDataIndexSupplementary(tipMessages[i]);
                 }
                 else if (messagefields[0] == "BDIm")
                 {
-                    BasicDataIndexMember bdim = new BasicDataIndexMember(items[i]);
+                    BasicDataIndexMember bdim = new BasicDataIndexMember(tipMessages[i]);
                 }
                 else if (messagefields[0] == "Is")
                 {
-                    IndexSummary indexsum = new IndexSummary(items[i]);
+                    IndexSummary indexsum = new IndexSummary(tipMessages[i]);
                 }
                 else if (messagefields[0] == "Iw")
                 {
-                    IndexWeight indexw = new IndexWeight(items[i]);
+                    IndexWeight indexw = new IndexWeight(tipMessages[i]);
                 }
                 else if (messagefields[0] == "TRh")
                 {
-                    CorporateAction trh = new CorporateAction(items[i]);
+                    CorporateAction trh = new CorporateAction(tipMessages[i]);
                 }
                 else if (messagefields[0] == "s")
                 {
-                    StateChange s = new StateChange(items[i]);
+                    StateChange s = new StateChange(tipMessages[i]);
                 }
                 else if (messagefields[0] == "r")
                 {
-                    OrderbookReferencePrice r = new OrderbookReferencePrice(items[i]);
+                    OrderbookReferencePrice r = new OrderbookReferencePrice(tipMessages[i]);
                 }
                 else if (messagefields[0] == "o")
                 {
-                    Orderbook1 o = new Orderbook1(items[i]);
+                    Orderbook1 o = new Orderbook1(tipMessages[i]);
                 }
                 else if (messagefields[0] == "p")
                 {
-                    Orderbook2 p = new Orderbook2(items[i]);
+                    Orderbook2 p = new Orderbook2(tipMessages[i]);
                 }
                 else if (messagefields[0] == "z")
                 {
-                    Orderbook3 z = new Orderbook3(items[i]);
+                    Orderbook3 z = new Orderbook3(tipMessages[i]);
                 }
                 else if (messagefields[0] == "u")
                 {
-                    TradeStatistics1 u = new TradeStatistics1(items[i]);
+                    TradeStatistics1 u = new TradeStatistics1(tipMessages[i]);
                 }
                 else if (messagefields[0] == "v")
                 {
-                    TradeStatistics2 v = new TradeStatistics2(items[i]);
+                    TradeStatistics2 v = new TradeStatistics2(tipMessages[i]);
                 }
                 else if (messagefields[0] == "w")
                 {
-                    TradeStatistics3 w = new TradeStatistics3(items[i]);
+                    TradeStatistics3 w = new TradeStatistics3(tipMessages[i]);
                 }
                 else if (messagefields[0] == "c")
                 {
-                    CallInformation1 c = new CallInformation1(items[i]);
+                    CallInformation1 c = new CallInformation1(tipMessages[i]);
                 }
                 else if (messagefields[0] == "Cl")
                 {
-                    CallInformation2 cl = new CallInformation2(items[i]);
+                    CallInformation2 cl = new CallInformation2(tipMessages[i]);
                 }
                 else if (messagefields[0] == "k")
                 {
-                    MBPOrderSnapshot k = new MBPOrderSnapshot(items[i]);
+                    MBPOrderSnapshot k = new MBPOrderSnapshot(tipMessages[i]);
                 }
                 else if (messagefields[0] == "t")
                 {
-                    Trade t = new Trade(items[i]);
+                    Trade t = new Trade(tipMessages[i]);
                 }
                 else if (messagefields[0] == "l")
                 {
-                    TurnoverListUpdate l = new TurnoverListUpdate(items[i]);
+                    TurnoverListUpdate l = new TurnoverListUpdate(tipMessages[i]);
                 }
                 else if (messagefields[0] == "i")
                 {
-                    IndexUpdate i1 = new IndexUpdate(items[i]);
+                    IndexUpdate i1 = new IndexUpdate(tipMessages[i]);
                 }
                 else if (messagefields[0] == "q")
                 {
-                    MarketMakerQuote1 q = new MarketMakerQuote1(items[i]);
+                    MarketMakerQuote1 q = new MarketMakerQuote1(tipMessages[i]);
                 }
                 else if (messagefields[0] == "y")
                 {
-                    MarketMakerQuote2 y = new MarketMakerQuote2(items[i]);
+                    MarketMakerQuote2 y = new MarketMakerQuote2(tipMessages[i]);
                 }
                 else if (messagefields[0] == "n")
                 {
-                    News n = new News(items[i]);
+                    News n = new News(tipMessages[i]);
                 }
                 else
                 {
-                    throw new Exception("Unknown TIP Message = " + items[i]);
+                    throw new Exception("Unknown TIP Message = " + tipMessages[i]);
                 }
             }
         }
